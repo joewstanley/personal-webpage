9a373344cbf55622c6dd5acbe38d6b600b81e925
Joseph Stanley <jwstanl3@gmail.com>
2018-03-11 18:14:59 -0400

    Added error pages

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'src')));
@@ -10,7 +11,19 @@ app.get('/', (req, res) => {
 
 app.get('/v1', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/views', 'v1.min.html'));
+});
+
+app.get('/page-not-found', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/views', 'notfound.min.html'));
+});
+
+app.get('/err', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/views', 'err.min.html'));
 })
+
+app.get('/*', (req, res) => {
+  res.redirect('/page-not-found');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
